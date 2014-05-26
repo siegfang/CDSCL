7bdbf196f6ff25fdffea24e6ed504384335534ff
siegfang <siegfang@gmail.com>
2014-05-26 18:05:22 +0800

    删除argparser

--- nut2/clscl.py
+++ nut2/clscl.py
@@ -6,107 +6,19 @@ __author__ = 'fangy'
 __version__ = "2.0"
 
 
-def train_args_parser():
-    """Create argument and option parser for the
-    training script.
-    """
-    description = """Prefixes `s_` and `t_` refer to source and target language
-    , resp. Train and unlabeled files are expected to be in Bag-of-Words format.
-    """
-    parser = argparse.ArgumentParser(usage="%clscl_train [options] " \
-                                           "s_lang t_lang s_train_file " \
-                                           "s_unlabeled_file t_unlabeled_file " \
-                                           "dict_file model_file",
-                                     # version="%prog " + __version__,
-                                     description=description)
-
-    parser.add_argument("-v", "--verbose",
-                        dest="verbose",
-                        help="verbose output",
-                        default=1,
-                        metavar="[0,1,2]",
-                        type=int)
-
-    parser.add_argument("-k",
-                        dest="k",
-                        help="dimensionality of cross-lingual representation.",
-                        default=100,
-                        metavar="int",
-                        type=int)
-
-    parser.add_argument("-m",
-                        dest="m",
-                        help="number of pivots.",
-                        default=450,
-                        metavar="int",
-                        type=int)
-
-    parser.add_argument("--max-unlabeled",
-                        dest="max_unlabeled",
-                        help="max number of unlabeled documents to read;" \
-                             "-1 for unlimited.",
-                        default=-1,
-                        metavar="int",
-                        type=int)
-
-    parser.add_argument("-p", "--phi",
-                        dest="phi",
-                        help="minimum support of pivots.",
-                        default=30,
-                        metavar="int",
-                        type=int)
-
-    parser.add_argument("-r", "--pivot-reg",
-                        dest="preg",
-                        help="regularization parameter lambda for " \
-                             "the pivot classifiers.",
-                        default=0.00001,
-                        metavar="float",
-                        type=float)
-
-    parser.add_argument("-a", "--alpha",
-                        dest="alpha",
-                        help="elastic net hyperparameter alpha.",
-                        default=0.85,
-                        metavar="float",
-                        type=float)
-
-    parser.add_argument("--strategy",
-                        dest="strategy",
-                        help="The strategy to compute the pivot classifiers." \
-                             "Either 'serial' or 'parallel' [default] or 'hadoop'.",
-                        default="parallel",
-                        metavar="str",
-                        type=str)
-
-    parser.add_argument("--n-jobs",
-                        dest="n_jobs",
-                        help="The number of processes to fork." \
-                             "Only if strategy is 'parallel'.",
-                        default=-1,
-                        metavar="int",
-                        type=int)
-
-    return parser
-
-
-def train():
+def train(arg_dict):
     """Training script for CLSCL.
 
     TODO: different translators.
     """
-    parser = train_args_parser()
-    args = parser.parse_args()
-    if len(args) != 7:
-        parser.error("incorrect number of arguments (use `--help` for help).")
 
-    slang = args[0]
-    tlang = args[1]
+    slang = arg_dict['s_lang']
+    tlang = arg_dict['t_lang']
 
-    fname_s_train = args[2]
-    fname_s_unlabeled = args[3]
-    fname_t_unlabeled = args[4]
-    fname_dict = args[5]
+    fname_s_train = arg_dict['s_labeled_file']
+    fname_s_unlabeled = arg_dict['s_unlabeled_file']
+    fname_t_unlabeled = arg_dict['t_unlabeled_file']
+    fname_dict = arg_dict['dict_file']
 
     # Create vocabularies
     # s_voc = bow.vocabulary(fname_s_train, fname_s_unlabeled,
