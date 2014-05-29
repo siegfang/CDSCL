58d936e86287dbeb706cb466b44f3e5e8c080f73
siegfang <siegfang@gmail.com>
2014-05-29 23:07:11 +0800

    add vector model argument

--- clscl_train.py
+++ clscl_train.py
@@ -5,118 +5,125 @@
 # License: BSD Style
 
 import nut2.clscl
-# from multiprocessing import freeze_support # use when run in Windows
+from multiprocessing import freeze_support
 
 import argparse
 
-
 def get_train_arg():
-    description = """Prefixes `s_` and `t_` refer to source and target language
+
+     description = """Prefixes `s_` and `t_` refer to source and target language
          , resp. Train and unlabeled files are expected to be in Bag-of-Words format.
          """
-    parser = argparse.ArgumentParser(  # version="%prog " + __version__,
-                                       description=description)
+     parser = argparse.ArgumentParser(# version="%prog " + __version__,
+                                      description=description)
 
-    parser.add_argument('s_lang',
-                        action="store",
-                        help="source language")
+     parser.add_argument('s_lang',
+                         action="store",
+                         help="source language")
 
-    parser.add_argument('t_lang',
-                        action="store",
-                        help="target language")
+     parser.add_argument('t_lang',
+                         action="store",
+                         help="target language")
 
-    parser.add_argument('s_labeled_file',
-                        action="store",
-                        help="source language labelled file path")
+     parser.add_argument('s_labeled_file',
+                         action="store",
+                         help="source language labelled file path")
 
-    parser.add_argument('s_unlabeled_file',
-                        action="store",
-                        help="source language unlabelled file path")
+     parser.add_argument('s_unlabeled_file',
+                         action="store",
+                         help="source language unlabelled file path")
 
-    parser.add_argument('t_unlabeled_file',
-                        action="store",
-                        help="target language unlabelled file path")
+     parser.add_argument('t_unlabeled_file',
+                         action="store",
+                         help="target language unlabelled file path")
 
-    parser.add_argument('dict_file',
-                        action="store",
-                        help="source to target language dictionary file path")
+     parser.add_argument('s_word2vec_file',
+                         action="store",
+                         help="the path stored the source word2vec model")
 
-    parser.add_argument('model_file',
-                        action="store",
-                        help="the path stored the model")
+     parser.add_argument('t_word2vec_file',
+                         action="store",
+                         help="the path stored the target word2vec model")
 
-    parser.add_argument("-v", "--verbose",
-                        dest="verbose",
-                        help="verbose output",
-                        default=1,
-                        metavar="[0,1,2]",
-                        type=int)
+     parser.add_argument('dict_file',
+                         action="store",
+                         help="source to target language dictionary file path")
 
-    parser.add_argument("-k",
-                        dest="k",
-                        help="dimensionality of cross-lingual representation.",
-                        default=100,
-                        metavar="int",
-                        type=int)
+     parser.add_argument('model_file',
+                         action="store",
+                         help="the path stored the model")
 
-    parser.add_argument("-m",
-                        dest="m",
-                        help="number of pivots.",
-                        default=450,
-                        metavar="int",
-                        type=int)
+     parser.add_argument("-v", "--verbose",
+                         dest="verbose",
+                         help="verbose output",
+                         default=1,
+                         metavar="[0,1,2]",
+                         type=int)
 
-    parser.add_argument("--max-unlabeled",
-                        dest="max_unlabeled",
-                        help="max number of unlabeled documents to read;" \
-                             "-1 for unlimited.",
-                        default=-1,
-                        metavar="int",
-                        type=int)
+     parser.add_argument("-k",
+                         dest="k",
+                         help="dimensionality of cross-lingual representation.",
+                         default=100,
+                         metavar="int",
+                         type=int)
 
-    parser.add_argument("-p", "--phi",
-                        dest="phi",
-                        help="minimum support of pivots.",
-                        default=30,
-                        metavar="int",
-                        type=int)
+     parser.add_argument("-m",
+                         dest="m",
+                         help="number of pivots.",
+                         default=450,
+                         metavar="int",
+                         type=int)
 
-    parser.add_argument("-r", "--pivot-reg",
-                        dest="preg",
-                        help="regularization parameter lambda for " \
-                             "the pivot classifiers.",
-                        default=0.00001,
-                        metavar="float",
-                        type=float)
+     parser.add_argument("--max-unlabeled",
+                         dest="max_unlabeled",
+                         help="max number of unlabeled documents to read;" \
+                              "-1 for unlimited.",
+                         default=-1,
+                         metavar="int",
+                         type=int)
 
-    parser.add_argument("-a", "--alpha",
-                        dest="alpha",
-                        help="elastic net hyperparameter alpha.",
-                        default=0.85,
-                        metavar="float",
-                        type=float)
+     parser.add_argument("-p", "--phi",
+                         dest="phi",
+                         help="minimum support of pivots.",
+                         default=30,
+                         metavar="int",
+                         type=int)
 
-    parser.add_argument("--strategy",
-                        dest="strategy",
-                        help="The strategy to compute the pivot classifiers." \
-                             "Either 'serial' or 'parallel' [default] or 'hadoop'.",
-                        default="parallel",
-                        metavar="str",
-                        type=str)
+     parser.add_argument("-r", "--pivot-reg",
+                         dest="preg",
+                         help="regularization parameter lambda for " \
+                              "the pivot classifiers.",
+                         default=0.00001,
+                         metavar="float",
+                         type=float)
 
-    parser.add_argument("--n-jobs",
-                        dest="n_jobs",
-                        help="The number of processes to fork." \
-                             "Only if strategy is 'parallel'.",
-                        default=-1,
-                        metavar="int",
-                        type=int)
-    arg_dict = vars(parser.parse_args())
-    return arg_dict
+     parser.add_argument("-a", "--alpha",
+                         dest="alpha",
+                         help="elastic net hyperparameter alpha.",
+                         default=0.85,
+                         metavar="float",
+                         type=float)
 
+     parser.add_argument("--strategy",
+                         dest="strategy",
+                         help="The strategy to compute the pivot classifiers." \
+                              "Either 'serial' or 'parallel' [default] or 'hadoop'.",
+                         default="parallel",
+                         metavar="str",
+                         type=str)
+
+     parser.add_argument("--n-jobs",
+                         dest="n_jobs",
+                         help="The number of processes to fork." \
+                              "Only if strategy is 'parallel'.",
+                         default=-1,
+                         metavar="int",
+                         type=int)
+     arg_dict = vars(parser.parse_args())
+     return arg_dict
 
 if __name__ == '__main__':
-    arg_dict = get_train_arg()
+     arg_dict = get_train_arg()
 
-    # freeze_support() # use when run in Windows
-    nut2.clscl.train(arg_dict)
+     freeze_support()
+     nut2.clscl.train(arg_dict)
--- make_vec.py
+++ make_vec.py
@@ -6,6 +6,7 @@ from xml.etree.ElementTree import iterparse
 
 import nltk
 import nut2.gensim as gensim
+import pdb
 
 def read_en_xml_file(item_xml_file):
 
@@ -19,17 +20,59 @@ def read_en_xml_file(item_xml_file):
             sentences.append(sentence)
     return sentences
 
+class XMLItemSentence(object):
+    def __init__(self, source, tokenizer):
+        """Simple format: one sentence = one line; words already preprocessed and separated by whitespace.
+
+        source can be either a string or a file object
+
+        Thus, one can use this for just plain files:
+
+            sentences = LineSentence('myfile.txt')
+
+        Or for compressed files:
+
+            sentences = LineSentence(bz2.BZ2File('compressed_text.bz2'))
+        """
+        self.source = source
+        self.tokenizer = tokenizer
+
+    def __iter__(self):
+        """Iterate through the lines in the source."""
+
+        # Assume it is a xml-like object and try treating it as such
+        # Things that don't have seek will trigger an exception
+        # self.source.seek(0)
+        for event, elem in iterparse(self.source):
+            if elem.tag == 'item':
+                text = elem.find('text').text
+                if text is None or len(text) < 1:
+                    continue
+                sentence = self.tokenizer.tokenize(text)
+                # pdb.set_trace()
+                if len(sentence) < 1:
+                    continue
+                yield sentence
+
+
 def read_de_xml_file(item_xml_file):
 
     sentences = []
+    wpt = nltk.tokenize.WordPunctTokenizer()
+    count = 0
     for event, elem in iterparse(item_xml_file):
         if elem.tag == 'item':
             text = elem.find('text').text
-            print text
-            sentence = nltk.tokenize.WordPunctTokenizer().tokenize(text)
+            if text is None or len(text) < 1:
+                continue
+            sentence = wpt.tokenize(text)
+            # pdb.set_trace()
             if len(sentence) < 1:
                 continue
             sentences.append(sentence)
+            count += 1
+            if count % 1000 == 0:
+                print count
     return sentences
 
 def read_sentence(corpus_file_path):
@@ -50,10 +93,10 @@ if __name__ == '__main__':
 
     # sentences = read_sentence(corpus_file_path)
     # sentences = read_en_xml_file(corpus_file_path)
-    sentences = read_de_xml_file(corpus_file_path)
+    # sentences = read_de_xml_file(corpus_file_path)
+    xis = XMLItemSentence(corpus_file_path, nltk.tokenize.WordPunctTokenizer())
 
-
-    print "%d sentence" % len(sentences)
+    # print "%d sentence" % len(sentences)
     # train word2vec on the two sentences
-    model = gensim.Word2Vec(sentences, min_count=1)
-    model.save(os.path.dirname(corpus_file_path) + '\mymodel')
+    model = gensim.Word2Vec(xis, min_count=5)
+    model.save(os.path.dirname(corpus_file_path) + '\mymodel', sep_limit=1000000)
